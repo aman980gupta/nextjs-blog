08621846e0ad7d20f733e7ca061253bdf76a83d7
aman980gupta <aman980gupta@gmail.com>
2024-03-14 00:07:03 +0530

    midleware fixed

--- src/app/api/logout/route.js
+++ src/app/api/logout/route.js
@@ -8,6 +8,6 @@ export const GET = async () => {
     const token = await responce.cookies.get("token")
     console.log(token)
 
-    await responce.cookies.set("token", "", { expiresIn: new Date(0) })
+    await responce.cookies.delete("token")
     return responce
 }
--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,26 @@
 import { NextResponse } from 'next/server'
- 
+
 // This function can be marked `async` if using `await` inside
+
 export function middleware(request) {
-    //const isPublic = ,"/blog/:path*"
-    let cookie = request.cookies.get('token')
-    console.log(cookie)
-  return NextResponse.redirect(new URL('/login', request.url))
-}
+  const  token = request.cookies.get('token')?.value || ""
+  //const reuwstURlOF = request.nextUrl.path
+  console.log("middleware is runnig")
+  const path = request.nextUrl.pathname
+  console.log(path)
+  const isPublicPathName = path === "/login" || path === "/register";
+  const isPrivate = path === "/blog/:path*" || path === "/contact/:path*" || path === "/movies/:path*" || path === "/" || path === "/logout";
+  if(!isPublicPathName && !token ){
+    return NextResponse.redirect(new URL("/login",request.nextUrl))
+  }
+  if(token && isPublicPathName ){
+    return NextResponse.redirect(new URL("/contact",request.nextUrl))
+  }
  
+  }
+
+
 export const config = {
-  matcher: ["/","/movies/:path*","/dynamic/:path*","/logout/:path*"]
+  matcher: ["/", "/movies/:path*", "/dynamic/:path*", "/logout", "/contact", "/about","/blog","/login","/register"],
+
 }
